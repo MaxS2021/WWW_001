bd23c7d3b1f332c3790296218e9d3f726e4b136c
solovov.ma <maxvmd@mail.ru>
2022-02-10 13:54:34 +0300

    news

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, url_for, request, render_template
+from flask import Flask, url_for, request, render_template, json
 
 app = Flask(__name__)
 
@@ -13,7 +13,14 @@ def index():
 
 @app.route('/odd_even')
 def odd_even():
-    return render_template('odd_even.html', number=3)
+    return render_template('odd_even.html', number=2)
+
+@app.route('/news')
+def news():
+    with open("news.json", "rt", encoding="utf8") as f:
+        news_list = json.loads(f.read())
+    print(news_list)
+    return render_template('news.html', news=news_list)
 
 
 if __name__ == '__main__':
